b8edefc07a8c4bbd616bdabf77d2512488590217
RubemFalco <rubemdneto@gmail.com>
2025-10-02 18:08:13 -0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,24 @@
+numero = 8
+
+
+while True:  
+    tentativa = ""
+    tentativas_restantes = 3
+    
+
+    while tentativa != numero and tentativas_restantes > 0:
+        tentativa = int(input("digite um numero aleatorio: "))
+        
+        if tentativa != numero:
+            tentativas_restantes -= 1
+            print(f"numero errado! Você ainda tem {tentativas_restantes} tentativa(s).")
+
+    if tentativa == numero:
+        print("voce acertou!")
+    else:
+        print("Número máximo de tentativas atingidas")
+
+    resposta = input("vamo novamente? (s/n): ")
+    if resposta.lower() != "s":
+        print("Encerrando o programa...")
+        break
