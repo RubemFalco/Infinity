d7eb8ac43abacd83e94105ba82360c29ce6983dd
RubemFalco <rubemdneto@gmail.com>
2025-10-02 20:21:54 -0300

    Add files via upload

---
+++ infinity.aula2.py
@@ -0,0 +1,50 @@
+#atividade 1
+# setfrutas = set()
+# setfrutas.add('maçã')
+# setfrutas.add('banana')
+# setfrutas.add('uva')
+# setfrutas.add('laranja')
+# setfrutas.add('morango')
+# print(setfrutas)
+
+#atividade 2
+# setfrutas = set()
+# setfrutas.add('morango')
+# setfrutas.add('uva')
+# print('uva' in setfrutas)
+
+#atividade 3
+# frutasvermelhas = set()
+# frutasvermelhas.add('morango')
+# frutasvermelhas.add('cereja')
+# frutasvermelhas.add('framboesa')
+# print(frutasvermelhas)
+
+#atividade 4
+# frutasvermelhas = set()
+# frutasvermelhas.add('morango')
+# frutasvermelhas.add('cereja')
+# frutasvermelhas.add('framboesa')
+# frutasvermelhas.remove('cereja')
+# print(frutasvermelhas)
+
+#atividade 5 & 6
+# set1= {'carro', 'moto', 'avião'}
+# set2= {'barco', 'jetski'}
+# print(set1|set2)
+# print(set1.union(set2))
+# set2.add('moto')
+# print(set1&set2)
+# print(set1.intersection(set2))
+
+#atividade 7
+# lista1 = ['maçã', 'banana', 'uva']
+# lista2 = ['cereja', 'morango', 'maçã']
+# lista1 = set(lista1)
+# lista2 = set(lista2)
+# print(lista1.union(lista2))
+
+dicionario1 = dict()
+dicionario1.update({'Roberto':'12'})
+print(dicionario1)
+print(dicionario1['Roberto'])
