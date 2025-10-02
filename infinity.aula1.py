86701ee6ec32083decab5898a80f28c989fceabb
RubemFalco <rubemdneto@gmail.com>
2025-10-02 19:04:43 -0300

    Add files via upload

---
+++ infinity.aula1.py
@@ -0,0 +1,30 @@
+#Atividade 1
+#lista numérica vazia, na qual foram adicionados 5 números utilizando o .append
+# lista1 = []
+# lista1.append(1)
+# lista1.append(2)
+# lista1.append(3)
+# lista1.append(4)
+# lista1.append(5)
+# print(lista1)
+
+#Atividade 2
+#lista de vogais
+# listavogais=['a','e','i', 'o', 'u']
+# print(listavogais)
+
+#Atividade 3
+#lista vazia onde foi adicionado um total de 3, cada um com uma classe diferente utilizando .append
+# lista1 = []
+# itemlista = ('abacaxi')
+# lista1.append(1)
+# lista1.append('string')
+# lista1.append(itemlista)
+# print(lista1)
+
+#Atividade 4
+#
+palestrante1 = ('Roberta', 'saúde', 'UPE')
+palestrante2 = ('Cláudio', 'TI', 'UNINASSAU')
+palestrante3 = ('Pacheco', 'Python', 'Católica')
+print(palestrante3)
